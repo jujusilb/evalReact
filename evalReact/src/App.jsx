d32f1fb149c18fd44d14a4cb3f79dbf9da0b0ef3
unknown <jujusilb@gmail.com>
2025-12-09 19:11:08 +0100

    mardi apres ecole

--- evalReact/src/App.jsx
+++ evalReact/src/App.jsx
@@ -1,9 +1,10 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import Presentation from './presentation';
 import FetchLevels from './fetch_levels';
 
 function App() {
-  let niveau =1
+  const {niveau} = useState(1);
+
 
   return (
     <>
--- evalReact/src/fetch_levels.jsx
+++ evalReact/src/fetch_levels.jsx
@@ -1,19 +1,29 @@
 import AffichageGrid from "./affichage_grid";
+import {useState, useEffect} from 'react'
 
-async function FetchLevels(niveau){
-    console.log("niveau", niveau.niveau);
-    let url = "http://localhost:4000/api/levels/" + niveau.niveau;
-    let response = await fetch(url);
-    let data =await response.json();
-    console.log("data")
-    console.log(data);
+function FetchLevels({niveau}){
+    const [data, setData] = useState(null)
+
+    useEffect(()=>{
+        async function fetchData(){
+            console.log("niveau", niveau.niveau);
+            let url = "http://localhost:4000/api/levels/" + niveau.niveau;
+            let response = await fetch(url);
+            let asyncData =await response.json();
+            setData(asyncData)
+            console.log("data")
+            console.log(data);
+        }
+        fetchData();
+    }, [niveau]);
     return (
-        <>
-           <p>niveau chargé !</p>
-           <table id="tableau">
-                            <AffichageGrid data={data}/>
-           </table>
-        </>
-    )
+    <>
+        <p>niveau chargé !</p>
+        <table id="tableau">
+                <AffichageGrid data={data}/>
+        </table>
+    </>
+  );
 }
+    
 export default FetchLevels
